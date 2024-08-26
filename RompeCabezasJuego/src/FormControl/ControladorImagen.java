90d3d84fad57d54e6e649bd4e15d45afc181a4a9
Leandro Alegre <88396808+Ranglerino@users.noreply.github.com>
2024-08-26 20:58:18 -0300

    Nuevos recursos

---
+++ RompeCabezasJuego/src/FormControl/ControladorImagen.java
@@ -0,0 +1,31 @@
+package FormControl;
+import java.awt.image.BufferedImage;
+public class ControladorImagen {
+	
+	
+	private BufferedImage[][] MatrizGanadora;
+	private BufferedImage[][] MatrizDesordenada;
+	public ControladorImagen() {
+		
+	}
+	
+	
+	 public BufferedImage[][] cortarImagen(BufferedImage imagen) {
+	        int filas = 3;
+	        int columnas = 3;
+	        int ancho = imagen.getWidth() / columnas;
+	        int alto = imagen.getHeight() / filas;
+
+	         this.MatrizGanadora = new BufferedImage[filas][columnas];
+	         
+	         // Aqui ocurre la magia
+	        for (int i = 0; i < filas; i++) {
+	            for (int j = 0; j < columnas; j++) {
+	            	this.MatrizGanadora[i][j] = imagen.getSubimage(j * ancho, i * alto, ancho, alto);
+	            }
+	        }
+
+	        return this.MatrizGanadora;
+	    }
+	
+}
---
+++ RompeCabezasJuego/src/View/PantallaImagen.java
@@ -0,0 +1,105 @@
+package View;
+
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import FormControl.Controlador;
+import FormControl.ControladorImagen;
+
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+
+public class PantallaImagen {
+	private ControladorImagen controladorImagen;
+	private JFrame frame;
+	private BufferedImage[][] MatrizImagenes;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PantallaImagen window = new PantallaImagen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PantallaImagen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		controladorImagen = new ControladorImagen();
+		frame.setBounds(100, 100, 705, 516);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		//declaracion de labels
+		JLabel imagenLabel = new JLabel("New label");
+		imagenLabel.setBounds(79, 45, 74, 45);
+		frame.getContentPane().add(imagenLabel);
+		
+		JComboBox<String> comboBox = new JComboBox<>(new String[] {"Imagen 1", "Imagen 2", "Imagen 3"});
+		//Listas simetricas
+		String [] rutaImagen = {"/resources/imagen1.png","/resources/imagen2.jpg","/resources/imagen3.jpg"};
+		comboBox.setBounds(90, 315, 126, 20);
+		frame.getContentPane().add(comboBox);
+		
+		JButton btnImagen = new JButton("Mostrar");
+		btnImagen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int index = comboBox.getSelectedIndex();
+				try {
+						//Leo la imagen dentro del proyecto
+						BufferedImage imagenOriginal = ImageIO.read(PantallaImagen.class.getResourceAsStream(rutaImagen[index]));
+						//le paso la imagen a mi logica de negocio y me devuelve una matriz con las imagenes
+						MatrizImagenes = controladorImagen.cortarImagen(imagenOriginal);
+						ImageIcon imageIcon = new ImageIcon(MatrizImagenes[0][0]); //PARSEO BUFFERED IMAGEN A IMAGEICON (obtengo la primera posicion de la matriz)
+						// Ahora ajusto el tamaño de la imagen para que se adapate a la jlabel
+						ImageIcon img = new ImageIcon(imageIcon.getImage().getScaledInstance(imagenLabel.getWidth(), imagenLabel.getHeight(), Image.SCALE_SMOOTH));
+						imagenLabel.setIcon(img); 
+				} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					
+					//ImageIcon ico = new ImageIcon(getClass().getResource("/resources/imagen1.png"));
+					//ImageIcon img = new ImageIcon(ico.getImage().getScaledInstance(imagenLabel.getWidth(), imagenLabel.getHeight(), Image.SCALE_SMOOTH));
+					//imagenLabel.setIcon(img);
+					
+				}
+				
+			}
+		});
+		btnImagen.setBounds(48, 348, 270, 57);
+		frame.getContentPane().add(btnImagen);
+		
+		
+		
+		
+	}
+}
