1afcad88928f627109a06a9323f79d222a4236c0
Leandro-codeProcesses <pagnatleandro@gmail.com>
2024-09-04 15:29:21 -0300

    redundancia mejorada y limpieza de codigo sin usar

--- RompeCabezasJuego/src/FormControl/ControladorImagen.java
+++ RompeCabezasJuego/src/FormControl/ControladorImagen.java
@@ -11,15 +11,15 @@ public class ControladorImagen {
 	private BufferedImage[][] MatrizOrdenada;
 	private int filaVacio = 2; // Posición inicial del casillero vacío
 	private int colVacio = 2;
+	
 	public ControladorImagen() {
 
 	}
 	
-	public  void imprimirGane() {
+	public  void imprimirGane() 
+	{
 		JOptionPane.showMessageDialog(null, "Juego finalizado, Ganaste!!!!!1!!1");
 	}
-
-
 
 	public void mezclarMatriz() 
 	{
@@ -29,45 +29,52 @@ public class ControladorImagen {
 		for (int cantidadMovimientosAleatorios = 0; cantidadMovimientosAleatorios < 1000; cantidadMovimientosAleatorios++) 
 		{
 			String movimiento = movimientos[rand.nextInt(movimientos.length)];
-			desplazarmeEnMatrizDesordenada(movimiento, 0);
+			desplazarmeEnMatriz(movimiento, 0);
 		}
 	}
-	public BufferedImage[][] cortarImagen(BufferedImage imagen) {
+	
+	public BufferedImage[][] cortarImagen(BufferedImage imagen,String ordenODesorden) 
+	{
 		int filas = 3;
 		int columnas = 3;
 		int ancho = imagen.getWidth() / columnas;
 		int alto = imagen.getHeight() / filas;
-
+		if(ordenODesorden.equals("a desordenar")) 
+		{
 		this.MatrizDesordenada = new BufferedImage[filas][columnas];
-		for (int i = 0; i < filas; i++) {
-			for (int j = 0; j < columnas; j++) {
+		for (int i = 0; i < filas; i++) 
+		{
+			for (int j = 0; j < columnas; j++) 
+			{
 				this.MatrizDesordenada[i][j] = imagen.getSubimage(j * ancho, i * alto, ancho, alto);
 			}
 		}
 		return this.MatrizDesordenada;
-	}
-	public BufferedImage[][] cortarImagenConOrden(BufferedImage imagen) {
-		int filas = 3;
-		int columnas = 3;
-		int ancho = imagen.getWidth() / columnas;
-		int alto = imagen.getHeight() / filas;
-
-
-		this.MatrizOrdenada = new BufferedImage[filas][columnas];
-		for (int i = 0; i < filas; i++) {
-			for (int j = 0; j < columnas; j++) {
-				this.MatrizOrdenada[i][j] = imagen.getSubimage(j * ancho, i * alto, ancho, alto);
+		}else if(ordenODesorden.equals("ordenada"))
+		{
+			this.MatrizOrdenada = new BufferedImage[filas][columnas];
+			for (int i = 0; i < filas; i++) 
+			{
+				for (int j = 0; j < columnas; j++) 
+				{
+					this.MatrizOrdenada[i][j] = imagen.getSubimage(j * ancho, i * alto, ancho, alto);
+				}
 			}
+			return this.MatrizOrdenada;
 		}
-		return this.MatrizOrdenada;
-	}	 
-	public int desplazarmeEnMatrizDesordenada(String tecla, int contador) {
+		return null;
+	}
+
+	public int desplazarmeEnMatriz(String tecla, int contador) 
+	{
 		int fila = this.filaVacio;
 		int columna = this.colVacio;
 		BufferedImage aux;
 		//Si se mueve a la derecha
-		if(tecla.equals("d")) {
-			if (columna < this.MatrizDesordenada[fila].length - 1) {
+		if(tecla.equals("d")) 
+		{
+			if (columna < this.MatrizDesordenada[fila].length - 1) 
+			{
 				aux = this.MatrizDesordenada[fila][columna + 1];
 				MatrizDesordenada[fila][columna + 1] = this.MatrizDesordenada[fila][columna];
 				MatrizDesordenada[fila][columna] = aux;
@@ -81,8 +88,10 @@ public class ControladorImagen {
 
 
 		//Si se mueve a la arriba
-		if(tecla.equals("w")) {
-			if (fila > 0) {
+		if(tecla.equals("w")) 
+		{
+			if (fila > 0) 
+			{
 				aux = this.MatrizDesordenada[fila - 1][columna];
 				this.MatrizDesordenada[fila - 1][columna] = this.MatrizDesordenada[fila][columna];
 				this.MatrizDesordenada[fila][columna] = aux;
@@ -96,16 +105,16 @@ public class ControladorImagen {
 
 
 		//Si se mueve a la abajo
-		if(tecla.equals("s")) {
-			if (fila < this.MatrizDesordenada.length - 1) {
+		if(tecla.equals("s")) 
+		{
+			if (fila < this.MatrizDesordenada.length - 1) 
+			{
 				aux = this.MatrizDesordenada[fila + 1][columna];
 				this.MatrizDesordenada[fila + 1][columna] = this.MatrizDesordenada[fila][columna];
 				this.MatrizDesordenada[fila][columna] = aux;
 				this.filaVacio++;
 				contador = incrementarContador(contador);
-
 				return contador;
-
 			}
 		}
 
@@ -121,55 +130,66 @@ public class ControladorImagen {
 				this.MatrizDesordenada[fila][columna] = aux;
 				this.colVacio--;
 				contador = incrementarContador(contador);
-
 				return contador;
 			}	
 		}
 		return contador;
 	}
-	private int incrementarContador(int contador) {
+	
+	private int incrementarContador(int contador) 
+	{
 		return ++contador;
 	}
-//	public boolean sonIconosIguales(ImageIcon test, ImageIcon test2) {
-//
-//		BufferedImage a=iconoABufferedImage(test.getImage());
-//		BufferedImage b=iconoABufferedImage(test2.getImage());
-//		for (int pixelX=0;pixelX<a.getWidth();pixelX++) {
-//			for (int pixelY=0;pixelY<a.getHeight();pixelY++) {
-//				if (a.getRGB(pixelX, pixelY)!=b.getRGB(pixelX, pixelY))
-//					return false;
-//			}
-//		}
-//
-//		return true;
-//	}
-	private boolean sonBufferedImagenIguales(BufferedImage imagenA, BufferedImage imagenB) {
+	//	public boolean sonIconosIguales(ImageIcon test, ImageIcon test2) {
+	//
+	//		BufferedImage a=iconoABufferedImage(test.getImage());
+	//		BufferedImage b=iconoABufferedImage(test2.getImage());
+	//		for (int pixelX=0;pixelX<a.getWidth();pixelX++) {
+	//			for (int pixelY=0;pixelY<a.getHeight();pixelY++) {
+	//				if (a.getRGB(pixelX, pixelY)!=b.getRGB(pixelX, pixelY))
+	//					return false;
+	//			}
+	//		}
+	//
+	//		return true;
+	//	}
+	private boolean sonBufferedImagenIguales(BufferedImage imagenA, BufferedImage imagenB) 
+	{
 		if(imagenA.getWidth()!=imagenB.getWidth()||imagenA.getHeight()!=imagenB.getHeight())
 			return false;
-		for (int pixelX = 0; pixelX < imagenA.getWidth(); pixelX++) {
-			for (int pixelY = 0; pixelY < imagenA.getHeight(); pixelY++) {
-				if (imagenA.getRGB(pixelX, pixelY) != imagenB.getRGB(pixelX, pixelY)) {
+		for (int pixelX = 0; pixelX < imagenA.getWidth(); pixelX++) 
+		{
+			for (int pixelY = 0; pixelY < imagenA.getHeight(); pixelY++) 
+			{
+				if (imagenA.getRGB(pixelX, pixelY) != imagenB.getRGB(pixelX, pixelY)) 
+				{
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-	public BufferedImage[][] getMatrizDesordenada() {
+	public BufferedImage[][] getMatrizDesordenada() 
+	{
 		return this.MatrizDesordenada;
 	}
-//	public static BufferedImage iconoABufferedImage(Image imagen)
-//	{
-//		BufferedImage newImage = new BufferedImage(imagen.getWidth(null), imagen.getHeight(null), BufferedImage.TYPE_INT_ARGB);//TYPE_INT_ARGB Represents an image with 8-bit RGBA color components packed intointeger pixels.
-//		Graphics2D hojaDeDibujoImagen = newImage.createGraphics();
-//		hojaDeDibujoImagen.drawImage(imagen, 0, 0, null);
-//		hojaDeDibujoImagen.dispose();
-//		return newImage;
-//	}
-	public boolean gano() {
-		for (int i = 0; i < MatrizDesordenada.length; i++) {
-			for (int j = 0; j < MatrizDesordenada[i].length; j++) {
-				if (!sonBufferedImagenIguales(MatrizDesordenada[i][j], MatrizOrdenada[i][j])) {
+	//	public static BufferedImage iconoABufferedImage(Image imagen)
+	//	{
+	//		BufferedImage newImage = new BufferedImage(imagen.getWidth(null), imagen.getHeight(null), BufferedImage.TYPE_INT_ARGB);//TYPE_INT_ARGB Represents an image with 8-bit RGBA color components packed intointeger pixels.
+	//		Graphics2D hojaDeDibujoImagen = newImage.createGraphics();
+	//		hojaDeDibujoImagen.drawImage(imagen, 0, 0, null);
+	//		hojaDeDibujoImagen.dispose();
+	//		return newImage;
+	//	}
+	
+	public boolean gano() 
+	{
+		for (int i = 0; i < MatrizDesordenada.length; i++) 
+		{
+			for (int j = 0; j < MatrizDesordenada[i].length; j++) 
+			{
+				if (!sonBufferedImagenIguales(MatrizDesordenada[i][j], MatrizOrdenada[i][j])) 
+				{
 					return false;
 				}
 			}
@@ -188,8 +208,5 @@ public class ControladorImagen {
 	public void setColVacio(int colVacio) {
 		this.colVacio = colVacio;
 	}
-	//	public BufferedImage[][] copiarMatriz(BufferedImage[][] matrizImagenOrdenada) {
-	//		// TODO Auto-generated method stub
-	//		return null;
-	//	}
+
 }
--- RompeCabezasJuego/src/View/PantallaImagen.java
+++ RompeCabezasJuego/src/View/PantallaImagen.java
@@ -28,7 +28,8 @@ public class PantallaImagen {
 	private JLabel [][] labels = new JLabel[3][3];
 	private int contador = 0;
 	
-	public void setRompecabezas(BufferedImage [][] MatrizImagenes, JLabel[][] labels) {
+	public void setRompecabezas(BufferedImage [][] MatrizImagenes, JLabel[][] labels) 
+	{
 		int xlabel = 190;
 		int ylabel = 70;
 		int auXlabel = 190;
@@ -38,7 +39,8 @@ public class PantallaImagen {
 		{
 			for (int columna = 0; columna < MatrizImagenes[fila].length; columna++) 
 			{
-				if(labels[fila][columna] == null) {
+				if(labels[fila][columna] == null) 
+				{
 					labels[fila][columna] = new JLabel();
 					labels[fila][columna].setBounds(xlabel, ylabel, 100, 100);
 					frame.getContentPane().add(labels[fila][columna]);
@@ -72,9 +74,12 @@ public class PantallaImagen {
 		}
 	}
 	
-	public void actualizarMatrizMostradaPorPantalla(BufferedImage [][] MatrizImagenes, JLabel[][] labels) {
-		for (int fila = 0; fila < MatrizImagenes.length; fila++) {
-			for (int columna = 0; columna < MatrizImagenes.length; columna++) {
+	public void actualizarMatrizMostradaPorPantalla(BufferedImage [][] MatrizImagenes, JLabel[][] labels) 
+	{
+		for (int fila = 0; fila < MatrizImagenes.length; fila++) 
+		{
+			for (int columna = 0; columna < MatrizImagenes.length; columna++) 
+			{
 				ImageIcon imageIcon = new ImageIcon(MatrizImagenes[fila][columna]); //PARSEO BUFFERED IMAGEN A IMAGEICON (obtengo la primera posicion de la matriz)
 				// Ahora ajusto el tamaño de la imagen para que se adapate a la jlabel
 				ImageIcon img = new ImageIcon(imageIcon.getImage().getScaledInstance(labels[fila][columna].getWidth(), labels[fila][columna].getHeight(), Image.SCALE_SMOOTH));
@@ -85,37 +90,18 @@ public class PantallaImagen {
 	}
 
 	/**
-	 * Launch the application.
-	 */
-
-	public void setVisiblePantalla(boolean condicion) {
-		frame.setVisible(condicion);
-	}
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PantallaImagen window = new PantallaImagen();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the application.
 	 */
-	public PantallaImagen() {
+	public PantallaImagen() 
+	{
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() 
+	{
 		frame = new JFrame();
 		controladorImagen = new ControladorImagen();
 		frame.setBounds(100, 100, 705, 516);
@@ -131,29 +117,22 @@ public class PantallaImagen {
 		JLabel labelImagenCompleta = new JLabel("");
 		labelImagenCompleta.setFocusable(false);
 		labelImagenCompleta.setBounds(559, 187, 120, 114);
-		frame.getContentPane().add(labelImagenCompleta);
-		//		JLabel lblNewLabel = new JLabel("New label");
-		//		lblNewLabel.setBounds(593, 187, 46, 14);
-		//		frame.getContentPane().add(lblNewLabel);
-
-//		JLabel labelOtraImagenCompleta2 = new JLabel("");
-//		labelOtraImagenCompleta2.setFocusable(false);
-//		labelOtraImagenCompleta2.setBounds(559, 11, 120, 114);
-//		frame.getContentPane().add(labelOtraImagenCompleta2);
 
 		JButton btnImagen = new JButton("Mostrar");
 		btnImagen.setFocusable(false);
-		btnImagen.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		btnImagen.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 
 				int index = comboBox.getSelectedIndex();
-				try {
+				try 
+				{
 					//Leo la imagen dentro del proyecto
 					BufferedImage imagenOriginal = ImageIO.read(PantallaImagen.class.getResourceAsStream(rutaImagen[index]));
 					//le paso la imagen a mi logica de negocio y me devuelve una matriz con las imagenes
-					MatrizImagenes = controladorImagen.cortarImagen(imagenOriginal);
-					MatrizImagenesOrdenadas = controladorImagen.cortarImagenConOrden(imagenOriginal);
-//					BufferedImage imagenCasilleroVacio= MatrizImagenes[controladorImagen.getFilaVacio()][controladorImagen.getColVacio()];
+					MatrizImagenes = controladorImagen.cortarImagen(imagenOriginal,"a desordenar");
+					MatrizImagenesOrdenadas = controladorImagen.cortarImagen(imagenOriginal,"ordenada");
 					controladorImagen.mezclarMatriz();
 					
 					setRompecabezas(MatrizImagenes, labels);
@@ -162,55 +141,9 @@ public class PantallaImagen {
 					ImageIcon icono = new ImageIcon(imagenOriginal);
 					ImageIcon iconoImagenCompleta = new ImageIcon(icono.getImage().getScaledInstance(labelImagenCompleta.getWidth(), labelImagenCompleta.getHeight(), Image.SCALE_SMOOTH));
 					labelImagenCompleta.setIcon(iconoImagenCompleta);
-				/*
-				 * 
-				 * 
-				 * CASOS DE PRUEBA COMPARANDO IMAGENES
-				 * 
-				 * 	
-				 */
-					//TEST DE IMAGENES IGUALES
-//					ImageIcon icono2 = new ImageIcon(imagenOriginal);
-//					ImageIcon otroIconoImagenCompleta = new ImageIcon(icono2.getImage().getScaledInstance(labelOtraImagenCompleta2.getWidth(), labelOtraImagenCompleta2.getHeight(), Image.SCALE_SMOOTH));
-//					labelOtraImagenCompleta2.setIcon(otroIconoImagenCompleta);
-//
-//
-//					
-//					ImageIcon test = (ImageIcon) labelImagenCompleta.getIcon();
-//					ImageIcon test2 = (ImageIcon) labelOtraImagenCompleta2.getIcon();
-//					System.out.println((controladorImagen.sonIguales(test,test2)));//@Expected(verify) = assertTrue(test1==test2)
-//					
-//					
-//					//TEST DE IMAGENES DISTINTAS
-//					//Leo dos imagenes del proyecto diferentes
-//					
-//					BufferedImage imagenOriginal2 = ImageIO.read(PantallaImagen.class.getResourceAsStream(rutaImagen[0]));
-//					BufferedImage imagenOriginal3 = ImageIO.read(PantallaImagen.class.getResourceAsStream(rutaImagen[1]));
-//					
-//					ImageIcon icono3 = new ImageIcon(imagenOriginal2);
-//					ImageIcon iconoImagenCompleta3 = new ImageIcon(icono3.getImage().getScaledInstance(labelImagenCompleta.getWidth(), labelImagenCompleta.getHeight(), Image.SCALE_SMOOTH));
-//					ImageIcon icono4 = new ImageIcon(imagenOriginal3);
-//					ImageIcon iconoImagenCompleta4 = new ImageIcon(icono4.getImage().getScaledInstance(labelOtraImagenCompleta2.getWidth(), labelOtraImagenCompleta2.getHeight(), Image.SCALE_SMOOTH));
-//					
-//					labelImagenCompleta.setIcon(iconoImagenCompleta3);
-//					
-//					labelOtraImagenCompleta2.setIcon(iconoImagenCompleta4);
-//					
-//					ImageIcon test3 = (ImageIcon) labelImagenCompleta.getIcon();
-//					ImageIcon test4 = (ImageIcon) labelOtraImagenCompleta2.getIcon();
-//					System.out.println((controladorImagen.sonIguales(test3,test4)));//@Expected(verify) = assertFalse(test3==test4)
-//					
-//					//TEST DE IGUALDAD DENUEVO (CHEQUEAR CONSOLA)
-//					labelImagenCompleta.setIcon(iconoImagenCompleta3);
-//					
-//					labelOtraImagenCompleta2.setIcon(iconoImagenCompleta3);
-//					
-//					ImageIcon test5 = (ImageIcon) labelImagenCompleta.getIcon();
-//					ImageIcon test6 = (ImageIcon) labelOtraImagenCompleta2.getIcon();
-//					System.out.println((controladorImagen.sonIguales(test5,test6)));//@Expected(verify) = assertTrue(test5==test6)
-//					
-//				
-				} catch (IOException e1) {
+					
+				} catch (IOException e1) 
+				{
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 
@@ -225,7 +158,8 @@ public class PantallaImagen {
 
 		JButton btnVolver = new JButton("Atras");
 		btnVolver.setFocusable(false);
-		btnVolver.addActionListener(new ActionListener() {
+		btnVolver.addActionListener(new ActionListener() 
+		{
 			public void actionPerformed(ActionEvent e) {
 				Menu m1 = new Menu();
 				m1.setVisiblePantalla(true);
@@ -241,38 +175,69 @@ public class PantallaImagen {
 
 		// INPUTS
 		// ESCUCHAR INPUTS DEL TECLADO
-		frame.addKeyListener(new KeyListener() {
+		frame.addKeyListener(new KeyListener() 
+		{
 			@Override
-			public void keyTyped(KeyEvent e) {
+			public void keyTyped(KeyEvent e) 
+			{
 				// TODO Auto-generated method stub
 
 			}
 			@Override
-			public void keyPressed(KeyEvent e) {
+			public void keyPressed(KeyEvent e) 
+			{
+
 				char tecla = e.getKeyChar();
 				String teclaS1 =String.valueOf(tecla);
 
-				contador = controladorImagen.desplazarmeEnMatrizDesordenada(teclaS1, contador);
+				contador = controladorImagen.desplazarmeEnMatriz(teclaS1, contador);
 				
 				actualizarMatrizMostradaPorPantalla(controladorImagen.getMatrizDesordenada(), labels);
 				labels[controladorImagen.getFilaVacio()][controladorImagen.getColVacio()].setIcon(null);
-//				
+		
 				comboBox.setVisible(false);
 				btnVolver.setVisible(false);
 				btnImagen.setVisible(false);
-				if (controladorImagen.gano()) {
+				if (controladorImagen.gano()) 
+				{
 //					Image imagenCasilleroVacio= MatrizImagenes[controladorImagen.getFilaVacio()][controladorImagen.getColVacio()].getScaledInstance(tecla, tecla, tecla);
 //					labels[controladorImagen.getFilaVacio()][controladorImagen.getColVacio()].setIcon((Icon) imagenCasilleroVacio);
 		            controladorImagen.imprimirGane();
 		            // cuando el jugador gana...
 		            btnVolver.setVisible(true);
 		        }
-				//				controladorImagen.gano(MatrizImagenes, MatrizImagenes);
 			}
 			@Override
-			public void keyReleased(KeyEvent e) {
+			public void keyReleased(KeyEvent e) 
+			{
 				// TODO Auto-generated method stub
 			}
 		});
 	}
+	/**
+	 * Launch the application.
+	 */
+
+	public void setVisiblePantalla(boolean condicion) 
+	{
+		frame.setVisible(condicion);
+	}
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					PantallaImagen window = new PantallaImagen();
+					window.frame.setVisible(true);
+				} catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 }
